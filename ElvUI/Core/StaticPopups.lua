888bcedabfe393984010d1622fb633ab67d7ccae
Luckyone <Fussel.13@web.de>
2021-04-01 01:48:40 +0200

    Download URL

--- ElvUI/Core/StaticPopups.lua
+++ ElvUI/Core/StaticPopups.lua
@@ -29,7 +29,7 @@ local STATICPOPUP_TEXTURE_ALERTGEAR = STATICPOPUP_TEXTURE_ALERTGEAR
 local YES, NO, OKAY, CANCEL, ACCEPT, DECLINE = YES, NO, OKAY, CANCEL, ACCEPT, DECLINE
 -- GLOBALS: ElvUIBindPopupWindowCheckButton
 
-local DOWNLOAD_URL = 'https://www.tukui.org/download.php?ui=elvui'
+local DOWNLOAD_URL = 'https://www.tukui.org/welcome.php' -- Redirect to landing page for now
 
 E.PopupDialogs = {}
 E.StaticPopup_DisplayedFrames = {}
