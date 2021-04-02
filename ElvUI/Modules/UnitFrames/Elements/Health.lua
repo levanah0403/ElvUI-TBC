0b0b32b4495eb96db1572a3c45182210bc9a051f
Azilroka <darktemplar9900@yahoo.com>
2021-04-01 23:01:06 -0500

    UF Health

--- ElvUI/Modules/UnitFrames/Elements/Health.lua
+++ ElvUI/Modules/UnitFrames/Elements/Health.lua
@@ -84,7 +84,6 @@ function UF:Configure_HealthBar(frame)
 	health.colorHealth = nil
 	health.colorClass = nil
 	health.colorReaction = nil
-	health.colorSelection = nil
 
 	if db.colorOverride and db.colorOverride == 'FORCE_ON' then
 		health.colorClass = true
@@ -96,11 +95,7 @@ function UF:Configure_HealthBar(frame)
 			health.colorHealth = true
 		end
 	else
-		if self.db.colors.healthselection then
-			health.colorSelection = true
-		--[[elseif self.db.colors.healththreat then
-			health.colorThreat = true]]
-		elseif self.db.colors.healthclass ~= true then
+		if self.db.colors.healthclass ~= true then
 			if self.db.colors.colorhealthbyvalue then
 				health.colorSmooth = true
 			else
