c274ab96422424d3871a1721ba27116094393f35
Luckyone <Fussel.13@web.de>
2021-04-11 01:15:28 +0200

    LootRoll

--- ElvUI/Modules/Misc/LootRoll.lua
+++ ElvUI/Modules/Misc/LootRoll.lua
@@ -117,7 +117,7 @@ local function CreateRollButton(parent, ntex, ptex, htex, rolltype, tiptext, ...
 end
 
 function M:CreateRollFrame()
-	local frame = CreateFrame('Frame', nil, E.UIParent)
+	local frame = CreateFrame('Frame', nil, E.UIParent, 'BackdropTemplare')
 	frame:Size(FRAME_WIDTH, FRAME_HEIGHT)
 	frame:SetTemplate()
 	frame:SetScript('OnEvent', OnEvent)
