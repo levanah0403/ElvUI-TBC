020684007858b53c4e8b5b05873bf7ba432773b8
Luckyone <Fussel.13@web.de>
2021-05-17 02:39:25 +0200

    Finish conjured_items table

--- ElvUI/Modules/Bags/Sort.lua
+++ ElvUI/Modules/Bags/Sort.lua
@@ -101,16 +101,18 @@ local inventorySlots = {
 }
 
 local conjured_items = {
-	-- Mage water
-	[5350] = true, -- Conjured Water
-	[2288] = true, -- Conjured Fresh Water (Level 5)
-	[2136] = true, -- Conjured Purified Water (Level 15)
-	[3772] = true, -- Conjured Spring Water (Level 25)
-	[8077] = true, -- Conjured Mineral Water (Level 35)
-	[8078] = true, -- Conjured Sparkling Water (Level 45)
-	[8079] = true, -- Conjured Crystal Water (Level 55)
+	-- Mage Water
+	[5350]	= true, -- Conjured Water
+	[2288]	= true, -- Conjured Fresh Water (Level 5)
+	[2136]	= true, -- Conjured Purified Water (Level 15)
+	[3772]	= true, -- Conjured Spring Water (Level 25)
+	[8077]	= true, -- Conjured Mineral Water (Level 35)
+	[8078]	= true, -- Conjured Sparkling Water (Level 45)
+	[8079]	= true, -- Conjured Crystal Water (Level 55)
+	[30703] = true, -- Conjured Mountain Spring Water (Level 60)
+	[22018] = true, -- Conjured Glacier Water (Level 65)
 
-	-- Mage food
+	-- Mage Food
 	[5349]	= true, -- Conjured Muffin
 	[1113]	= true, -- Conjured Bread (Level 5)
 	[1114]	= true, -- Conjured Rye (Level 15)
@@ -118,14 +120,16 @@ local conjured_items = {
 	[8075]	= true, -- Conjured Sourdough (Level 35)
 	[8076]	= true, -- Conjured Sweet Roll (Level 45)
 	[22895] = true, -- Conjured Cinnamon Roll (Level 55)
+	[22019] = true, -- Conjured Croissant (Level 65)
 
-	-- Mage mana
-	[5514] = true, -- Mana Agate
-	[5513] = true, -- Mana Jade
-	[8007] = true, -- Mana Citrine
-	[8008] = true, -- Mana Ruby
+	-- Mage Mana
+	[5514]	= true, -- Mana Agate
+	[5513]	= true, -- Mana Jade
+	[8007]	= true, -- Mana Citrine
+	[8008]	= true, -- Mana Ruby
+	[22044] = true, -- Mana Emerald
 
-	-- Warlock soulstones
+	-- Warlock Soulstones
 	[5232]	= true, -- Minor Soulstone
 	[16892] = true, -- Lesser Soulstone
 	[16893] = true, -- Soulstone
@@ -133,11 +137,12 @@ local conjured_items = {
 	[16896] = true, -- Major Soulstone
 	[22116] = true, -- Master Soulstone
 
-	-- Warlock firestones
+	-- Warlock Firestones
 	[1254]	= true, -- Lesser Firestone
 	[13699] = true, -- Firestone
 	[13700] = true, -- Greater Firestone
 	[13701] = true, -- Major Firestone
+	[22128] = true, -- Master Firestone
 
 	-- Warlock Spellstones
 	[5522]	= true, -- Spellstone
@@ -145,7 +150,7 @@ local conjured_items = {
 	[13603] = true, -- Major Spellstone
 	[22646] = true, -- Master Spellstone
 
-	-- Warlock healthstones
+	-- Warlock Healthstones
 	[5512]	= true, -- Minor Healthstone
 	[19004] = true, -- Minor Healthstone (1/2 Improved Healthstone)
 	[19005] = true, -- Minor Healthstone (2/2 Improved Healthstone)
