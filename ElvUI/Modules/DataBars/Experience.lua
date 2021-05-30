23031f25a22ff7ab975343d962ab1b7e2b6e3889
Repooc <tjcooper1982@gmail.com>
2021-05-29 21:48:42 -0400

    @luckyone make the option

--- ElvUI/Modules/DataBars/Experience.lua
+++ ElvUI/Modules/DataBars/Experience.lua
@@ -118,7 +118,7 @@ function DB:ExperienceBar_QuestXP()
 	QuestLogXP = 0
 	DB:ExperienceBar_CheckQuests(bar.db.questCurrentZoneOnly, bar.db.questCompletedOnly)
 
-	if QuestLogXP > 0 then
+	if bar.db.showQuestXP and QuestLogXP > 0 then
 		bar.Quest:SetMinMaxValues(0, XPToLevel)
 		bar.Quest:SetValue(min(CurrentXP + QuestLogXP, XPToLevel))
 		bar.Quest:SetStatusBarColor(DB.db.colors.quest.r, DB.db.colors.quest.g, DB.db.colors.quest.b, DB.db.colors.quest.a)
